335aa435b8914a12d6020ceeea4137cf2f54dc73
John A <94640025+JohnAisthorpe@users.noreply.github.com>
2023-10-05 00:06:38 +0100

    added basic html file

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from controllers import controller
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# @app.route('/<name>') 
+# def greet(name): 
+#     return f"Hello {name}!" 
+
+    
--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,3 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
-def index():
-    return "Hello, World!"
-
-@app.route('/<name>') 
-def greet(name): 
-    return f"Hello {name}!" 
